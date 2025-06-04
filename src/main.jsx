845d04dff8b1fd54c20fb7340cbf2b25fd53a376
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-06-04 09:59:48 +0100

    Modfied component structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Header from "./components/header.jsx";
-import CVapp from "./components/cv.jsx";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Header />
-    <CVapp />
+    <App />
   </StrictMode>
 );
