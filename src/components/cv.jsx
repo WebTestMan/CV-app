302fcbf177a8d7a43c9dd3c23822dd360b819099
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-06-04 16:31:59 +0100

    moved work history form to cv app

--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -1,8 +1,28 @@
-export default function CVapp (){
-    return (<div>
-        <div>
-            <h2>Start you CV here...</h2>
-        </div>
+export default function CVapp() {
+  return (
+    <div>
+      <div>
+        <h3>Start you CV here...</h3>
+      </div>
     </div>
-        )
+  );
+}
+
+// take form input and add it to the workHistoryArray
+// id: crypto.randomUUID()
+// job title
+// employer name
+// date started
+// date ended
+// description
+
+function AddWorkHistory() {
+  return (
+    <div>
+      <form>
+        <label>Job Title</label>
+        <input />
+      </form>
+    </div>
+  );
 }
