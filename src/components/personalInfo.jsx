d220c65c1a6e3fa077f0cb5232cdae00cb809e95
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-06-04 17:44:54 +0100

    started personal info section

---
+++ src/components/personalInfo.jsx
@@ -0,0 +1,10 @@
+export default function PersonalInfo() {
+  return (
+    <div>
+      <h2>Steve Works</h2>
+      <p>Telephone: 01112345</p>
+
+      <p>Email: email@email.com</p>
+    </div>
+  );
+}
