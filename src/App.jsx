719721f7af2ecf0f876730857b8626096bca96ba
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-06-04 16:32:56 +0100

    layout and style changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,22 @@ import Header from "./components/header.jsx";
 import CVapp from "./components/cv.jsx";
 import WorkHistory from "./components/workHistory.jsx";
 import "./styles/index.css";
+import "./styles/mobile.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <CVapp />
-      <WorkHistory />
+    <div className="pageLayout">
+      <div className="header">
+        <Header />
+      </div>
+
+      <div className="sidebar">
+        <CVapp />
+      </div>
+
+      <div className="mainContent">
+        <WorkHistory />
+      </div>
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import "../styles/header.css"
 export default function Header() {
   return (
     <div>
-      <header className="header">This is a header</header>
+      <header>This is a header</header>
       <p>This is the header for a CV app</p>
     </div>
   );
