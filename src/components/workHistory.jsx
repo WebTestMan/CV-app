e375e0be169d8f1ee261dcf58263ad83e38ef409
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-06-04 16:32:22 +0100

    various changes to implement work history

--- src/components/workHistory.jsx
+++ src/components/workHistory.jsx
@@ -1,47 +1,64 @@
 import React from "react";
+import { format } from "date-fns";
+import "../styles/workHistory.css";
 
 function ListItem(props) {
-  return <li>{props.work}</li>;
+  const id = props.id;
+  const jobTitle = props.jobTitle;
+  const jobDesc = props.jobDesc;
+  const startDate = props.startDate;
+
+  return (
+    <li key={id} className="work-history-list">
+      <div>
+        <h3>{jobTitle}</h3>
+        <p>{startDate}</p>
+        <p>{jobDesc}</p>
+      </div>
+      <br></br>
+    </li>
+  );
 }
 
 function List(props) {
   return (
     <ul>
       {props.workHistoryArray.map((work) => {
-        return <ListItem key={work} work={work} />;
+        return (
+          <ListItem
+            key={work.id}
+            jobTitle={work.jobTitle}
+            startDate={work.startDate}
+            jobDesc={work.jobDesc}
+          />
+        );
       })}
     </ul>
   );
 }
 
-// take form input and add it to the workhistoryarray
-// id: crypto.randomUUID()
-// job title
-// employer name
-// date started
-// date ended
-// description
-
-
-function AddWorkHistory() {
-  return (
-    <div>
-      <form>
-        <label>Job Title</label>
-        <input />
-      </form>
-    </div>
-  );
-}
-
 export default function WorkHistory() {
-  const workHistoryIndexArray = ["a job"];
+  const workHistoryIndexArray = [
+    {
+      id: crypto.randomUUID(),
+      jobTitle: "More recent Job Title",
+      jobDesc: "This was a job I did once, a long time ago.",
+      startDate: format(new Date("2024", "10", "2"), "dd/MM/yyyy"),
+      endDate: format(new Date("2025", "21", "09"), "dd/MM/yyyy"),
+    },
+    {
+      id: crypto.randomUUID(),
+      jobTitle: "Sample Job Title",
+      jobDesc: "This was a job I did once, a long time ago.",
+      startDate: format(new Date("2012", "0", "2"), "dd/MM/yyyy"),
+      endDate: format(new Date("2024", "21", "09"), "dd/MM/yyyy"),
+    },
+  ];
 
   return (
-    <div>
-      <h1>Work History: </h1>
+    <div className=".work-history-form">
+      <h2>Work History: </h2>
       <List workHistoryArray={workHistoryIndexArray} />
-      <AddWorkHistory />
     </div>
   );
 }
